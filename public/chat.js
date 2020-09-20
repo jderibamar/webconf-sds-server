8b0f844a9e4ec298015b9dc7453804d675bc1efa
jderibamar <jjrrdd@gmail.com>
2020-09-20 02:47:43 -0300

    up server socket.io

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 $( function()
 { 
-    var socket = io.connect('https://webconf-sds-server.herokuapp.com')
+    socket = io.connect('https://webconf-sds-server.herokuapp.com/')
     // socket = io('http://localhost:3000')
     var ready = false
    
